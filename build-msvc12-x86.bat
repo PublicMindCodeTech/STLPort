c6a3713e6f5c618d8bd20757490a3e356cc93241
Public MindCodeTech <public@mindcodetech.com>
2013-10-13 08:33:46 +0100

    completed stlport configuration

--- build-msvc12-x86.bat
+++ build-msvc12-x86.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 call configure.bat msvc12
 call cd build\lib
 call nmake clean install
